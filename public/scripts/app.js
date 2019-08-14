fccd4ca44eb84a8be095329dd4efe248389aa3a3
Jackson Fung <jacksonf21@hotmail.com>
2019-08-14 14:20:51 +0000

    add chronology with GET tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,6 @@
  */
 
 $(document).ready(()=> {
-  let data;
   const promise1 = () => {
     return new Promise((resolve, reject) => {
       $.getJSON('http://localhost:8080/tweets/', (data) => {
@@ -13,51 +12,95 @@ $(document).ready(()=> {
       });
     });
   };
-  const container = document.getElementById('tweet-feed');
-  
-  promise1()
-    .then((data) => {
-      data.forEach(elem => {
-        console.log(elem);
-        $(container).append(
-          `<article>
-            <content>
-              <div id='name'>
-                <img src=${elem.user.avatars}>
-                <text>${elem.user.name}</text>
-              </div>
-              <div id='avatar'>
-              </div>
-              <div id='handle'>
-                ${elem.user.handle}
-              </div>
-              <div id='message'>
-                ${elem.content.text}
-              </div>
-            </content>
-            <footer>
-              <text>${date(elem)}</text>
-              <div id='icons'>
-                placeholder
-              </div>
-            </footer>
-          </article>`
-        );
-      });
-    });
+  // const container = document.getElementById('tweet-feed');
+  // const renderTweet = (tweet) => {
+  //   appendTweet(tweet);
+  // };
 
-  const date = (elem) => {
-    const plural = 's';
-    //Plural
-    if ()
-    //Not plural
-
-    //case return
-    if (Math.round((Date.now() - elem.created_at) / 86400000) === 0) {
-      return 'Today';
+  const appendTweet = (tweet, num = 0) => {
+    const tweetHTML = `<article>
+                        <content>
+                          <div id='name'>
+                            <img src=${tweet.user.avatars}>
+                            <text>${tweet.user.name}</text>
+                          </div>
+                          <div id='avatar'>
+                          </div>
+                          <div id='handle'>
+                            ${tweet.user.handle}
+                          </div>
+                          <div id='message'>
+                            ${tweet.content.text}
+                          </div>
+                        </content>
+                        <footer>
+                          <text>${date(tweet)}</text>
+                          <div id='icons'>
+                            placeholder
+                          </div>
+                        </footer>
+                      </article>`;
+    if (num === 1) {
+      $('#tweet-feed').prepend(tweetHTML);
+    } else {
+      $('#tweet-feed').append(tweetHTML);
     }
-    return `${Math.round((Date.now() - elem.created_at) / 86400000)} days ago`;
   };
 
-  const 
+  promise1()
+    .then((data) => data.forEach(elem => appendTweet(elem)));
+
+  $('.new-tweet').submit(function(event) {
+    event.preventDefault();
+    let tweet = encodeURI($('.new-tweet textarea').serialize());
+    console.log(tweet);
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:8080/tweets',
+      data: tweet,
+      success: () => {
+        $.get('http://localhost:8080/tweets', (data) => {
+          appendTweet(data[0], 1);
+        });
+      }
+    });
+
+  });
+
+  const date = (elem) => {
+    let ms = (Date.now() - elem.created_at);
+    let plural = '';
+    let unit = '';
+    let denom = 0;
+
+    //Plural Ternary
+    ms >= 0 && ms < 2000 ? plural = '' :
+    ms >= 60000 && ms < 120000 ? plural = '' :
+    ms >= 3600000 && ms < 7200000 ? plural = '' :
+    plural = 's';
+
+    //Unit selection
+    ms >= 0 && ms < 60000 ? unit = `second${plural} ago` :
+    ms >= 60000 && ms < 3600000 ? unit = `minute${plural} ago` :
+    ms >= 3600000 && ms < 86400000 ? unit = `hour${plural} ago` :
+    unit = `day${plural} ago`;
+
+    ms >= 0 && ms < 60000 ? denom = 1000 :
+    ms >= 60000 && ms < 3600000 ? denom = 60000 :
+    ms >= 3600000 && ms < 86400000 ? denom = 3600000 :
+    denom = 86400000;
+
+    //case return
+    return `${Math.round((Date.now() - elem.created_at) / denom)} ${unit}`;
+  };
+
 });
+
+
+
+
+// $(tweet).submit((event) => {
+//   event.preventDefault();
+
+// });
